834830f554c3209194ca9ee01bf21ebf792d3f65
Jim-Boba <jldavisjr@yahoo.com>
2023-12-04 19:15:27 -0500

    day 4 part 2

--- src/Day 4/day4.py
+++ src/Day 4/day4.py
@@ -22,17 +22,39 @@ with open("input.txt") as file:
         
         plays = plays.split(" ")
         score_card = score_card.split(" ")
-        initial_tickets.append( list( (game_num,score_card,plays) ) )
+        initial_tickets.append( list( (game_num,score_card,plays,line_no,1) ) )
         for play in plays:
             if play in score_card and play != '':
                 inc_score()
         total += round_score
         line_no += 1
-
+    
     #total_tickets = len(initial_tickets)
     ticket_queue = initial_tickets
-    while len(ticket_queue) > 0:
-        print()
+    #while len(ticket_queue) > 0:
+    for ticket in ticket_queue:
+        game_num = ticket[0]
+        score_card = ticket[1]
+        plays = ticket[2]
+        line = ticket[3]
+        num = ticket[4]
+        score = 0
+        for play in plays:
+            if play in score_card and play != '':
+                score += 1
+        print(num)
+        
+        length = len(initial_tickets)
+        if( line < length ):
+            for ticket in ticket_queue[line:min(length,line+score)]:
+                ticket[4] += 1*num
+        
+        line_no += 1
+
+for ticket in ticket_queue:
+    total_tickets += ticket[4]
 
 print(total)
 print(total_tickets)
+length = len(initial_tickets)
+#print( int((length **2 + length)/2))
